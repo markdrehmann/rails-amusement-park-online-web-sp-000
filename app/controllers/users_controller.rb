759ccc31c9b0b4b900a7ab2311cd56b232e9d69f
markdrehmann <markdrehmann@gmail.com>
2021-02-08 15:49:32 -0600

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            # display errors?
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        if logged_in? && @user == current_user
+            # cool
+        else
+            # error
+            redirect_to '/'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :height, :tickets, :happiness, :nausea, :admin)
+    end
+end
